009a7d5ffa2471f82774ce015fdb08a14ffc0481
Adedoyin <dbizzyxy@gmail.com>
2026-01-24 17:11:47 +0100

    chore: configure nuxt for shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,29 @@
-
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  future: { compatibilityVersion: 4 },
 
-  vite: { 
-    plugins: [ 
-      tailwindcss()
-    ],
+  modules: [
+    'shadcn-nuxt',
+    'nuxt-lucide-icons',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt',
+  ],
+
+  shadcn: {
+    prefix: 'Shad',
+    componentDir: './shadcn/components/ui',
+  },
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 })
