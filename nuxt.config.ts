a9644b785497a735476c90930785f58a3eaea5a7
Adedoyin <dbizzyxy@gmail.com>
2026-01-25 23:21:30 +0100

    chore: add app head config with favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,22 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
 
+  app: {
+    head: {
+      title: 'Piczel',
+      meta: [
+        { name: 'description', content: 'Photo gallery management for photographers' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+
   typescript: {
     tsConfig: {
       include: ['../shadcn/**/*'],
